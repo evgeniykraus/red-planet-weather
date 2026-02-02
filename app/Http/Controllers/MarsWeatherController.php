191aa241a77af2b005688d2bc3551f29cbd01910
Евгений Краус <evgeniykraus@gmail.com>
2026-02-02 15:26:03 +0300

    feat: добавлены контроллер и маршруты для статистики погоды

---
+++ app/Http/Controllers/MarsWeatherController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\MonthStatisticsRequest;
+use App\Services\MarsWeatherService;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class MarsWeatherController extends Controller
+{
+    public function __construct(
+        private readonly MarsWeatherService $marsWeatherService
+    ) {}
+
+    public function index(): Response
+    {
+        return Inertia::render('MarsWeather', [
+            'availableMonths' => range(1, 12),
+        ]);
+    }
+
+    public function getMonthStatistics(MonthStatisticsRequest $request): Response
+    {
+        $validated = $request->validated();
+        $marsMonth = (int) $validated['mars_month'];
+        $statistics = $this->marsWeatherService->getMonthStatistics($marsMonth);
+
+        return Inertia::render('MarsWeather', [
+            'statistics' => $statistics,
+            'selectedMonth' => $marsMonth,
+            'availableMonths' => range(1, 12),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\MarsWeatherController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+Route::get('/', [MarsWeatherController::class, 'index'])->name('mars.index');
+Route::get('/statistics', [MarsWeatherController::class, 'getMonthStatistics'])->name('mars.statistics');
